6a9ed01d6f49fd1e8cc51e4a5bfb89129e45983d
larsg <lars.gunnar@jobloop.no>
2024-11-05 09:04:59 +0100

    switch to main style Program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+    var app = builder.Build();
 
-// Setup static file hosting
-app.UseDefaultFiles();
-app.UseStaticFiles();
+    // Setup static file hosting
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
 
-// Status endpoint
-app.MapGet("/api/status", () => "Server healthy!");
+    // Status endpoint
+    app.MapGet("/api/status", () => "Server healthy!");
 
-app.Run();
+    app.Run();
+  }
+}
